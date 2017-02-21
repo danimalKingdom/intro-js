e6b92ed13364484bec8a45ecd03415242f4480bc
dani <dani.zaghian@gmail.com>
2017-02-20 21:09:18 -0800

    conditionals and loops

---
+++ conditionals_and_loops.js
@@ -0,0 +1,108 @@
+// RUBY CONDITIONAL
+// x = 20
+// if x > 50
+//  puts "high number"
+// elsif x == 30
+//  puts "my favorite number"
+// else
+//  puts "low number"
+// end
+
+// JS CONDITIONAL
+// var x = 51;
+// if (x > 50) {
+//    console.log("high number");  
+// } else if (x == 30) {
+//    console.log("my favorite number");
+// } else {
+//    console.log("low number"); 
+// }
+
+
+// RUBY EQUALITY
+// 5 == 5 #true
+// 5 == "5" #false
+// 5 != "5" #true
+
+// JS EQUALITY
+// console.log(5 == 5); //true
+// console.log(5 == "5"); //true
+// console.log(5 === "5"); //false
+// console.log([5] == 5); //true
+// console.log(5 !== "5"); //true
+// console.log(5 != "5"); //false
+
+
+// RUBY FALSEYS
+// nil
+// false
+
+// JAVASCRIPT FALSEYS
+// if (!false) {console.log("WAT")};
+// if (!undefined) {console.log("WAT")};
+// if (!null) {console.log("WAT")};
+// if (!NaN) {console.log("WAT")};
+// if (!0) {console.log("WAT")};
+// if (!"") {console.log("WAT")};
+
+
+// RUBY RAND NUMS
+// rand(1..100)
+
+// JS RAND NUMS
+// console.log(Math.floor(Math.random() * 100));
+
+
+
+// RUBY WHILE LOOP
+// i = 0
+// text = ""
+// while i < 10
+//  text = text + "The number is " + i.to_s
+//  i += 1
+// end
+// puts text
+
+//JS WHILE LOOP
+// var i = 0;
+// var text = "";
+// while (i<10) {
+//  text = text + "The number is " + i;
+//  i++;
+// }
+// console.log(text);
+
+
+// RUBY FOR LOOP
+// for i in 0...10
+//  puts i
+// end
+
+//JS FOR LOOP
+// for (var i = 0; i < 10; i++) {
+//  console.log(i);
+// }
+
+
+// RUBY EACH THROUGH ARRAY
+// fruits = ["banana", "apple", "orange", "mango", "kiwi"]
+// fruits.each do |fruit|
+//  puts fruit
+// end
+
+// JS EACH THROUGH ARRAY
+// var fruits = ["banana", "apple", "orange", "mango", "kiwi"];
+// for (var i = 0; i < fruits.length; i++) {
+//  console.log(fruits[i]);  
+// }
+
+
+
+
+
+
+
+
+
+
+
